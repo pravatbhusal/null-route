3c08bcdcd8a0b8dae7829d183db3e27fa82b0674
Pravat Bhusal <pravat.bhusal@gmail.com>
2019-01-09 19:22:03 -0600

    No longer null route based on ports

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # an interval to run the script every "run_interval" seconds
 run_interval = 5
 
-# size of each ip list
-ip_list_size = 3
+# size of the ip list
+ip_list_size = 25
 
-# ports to limit using a maximum number of connections
-ports = {"80": 20, "8080": 20, "25": 20, "22": 20, "5432": 20, "47623": 20, "47624": 20}
+# maximum amount of connections per ip address
+limit = 20
 
 # the security ISP your server uses for DDoS protection; do not change if the server is not using a security ISP
 secure_isp = "Cloudflare"
